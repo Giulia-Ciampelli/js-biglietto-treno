b2f6cd6cd7242ceb510afc08e1cad6ff3c578634
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-09-30 19:10:07 +0200

    docs: other option

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,19 +15,12 @@ basePrice = distance * 0.21;
 // sconto minorenni
 if (age < 18) {
     basePrice = basePrice - (basePrice * 0.2);
-    console.log(`Sconto per minorenni: ${basePrice.toFixed(2)} €`);
-    alert(`Sconto per minorenni: ${basePrice.toFixed(2)} €`);
 }
 
 // sconto pensionati
 else if (age >= 65) {
     basePrice = basePrice - (basePrice * 0.4);
-    console.log(`Sconto per pensionati: ${basePrice.toFixed(2)} €`);
-    alert(`Sconto per pensionati: ${basePrice.toFixed(2)} €`);
 }
 
-// prezzo pieno
-else {
-    console.log(`Prezzo pieno: ${basePrice.toFixed(2)} €`);
-    alert(`Prezzo pieno: ${basePrice.toFixed(2)} €`);
-}
+// output
+alert(`Il tuo biglietto costa: ${basePrice.toFixed(2)} €`);
