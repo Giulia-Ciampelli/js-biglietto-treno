9f7d1010456bdfdd0d3c31e9762dd1e6d9cf1191
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-09-30 18:20:14 +0200

    fix: tutor tip

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 // dichiarazione variabili
 let basePrice;
-let minorDiscount;
-let seniorDiscount;
 let age;
 let distance;
 
@@ -12,20 +10,24 @@ distance = Number(prompt('quanti km devi fare?'));
 // calcolo prezzo per km
 basePrice = distance * 0.21;
 
-// calcolo sconto prezzi per età
-minorDiscount = basePrice - (basePrice * 0.2);
-seniorDiscount = basePrice - (basePrice * 0.4);
+// elaborazione dati con calcolo discount
 
-// elaborazione dati
+// sconto minorenni
 if (age < 18) {
-    console.log(`Sconto per minorenni: ${minorDiscount.toFixed(2)} €`);
-    alert(`Sconto per minorenni: ${minorDiscount.toFixed(2)} €`);
+    basePrice = basePrice - (basePrice * 0.2);
+    console.log(`Sconto per minorenni: ${basePrice.toFixed(2)} €`);
+    alert(`Sconto per minorenni: ${basePrice.toFixed(2)} €`);
 }
-if (age >= 18 && age < 65) {
+
+// sconto pensionati
+else if (age >= 65) {
+    basePrice = basePrice - (basePrice * 0.4);
+    console.log(`Sconto per pensionati: ${basePrice.toFixed(2)} €`);
+    alert(`Sconto per pensionati: ${basePrice.toFixed(2)} €`);
+}
+
+// prezzo pieno
+else {
     console.log(`Prezzo pieno: ${basePrice.toFixed(2)} €`);
     alert(`Prezzo pieno: ${basePrice.toFixed(2)} €`);
-}
-if (age >= 65) {
-    console.log(`Sconto per pensionati: ${seniorDiscount.toFixed(2)} €`);
-    alert(`Sconto per pensionati: ${seniorDiscount.toFixed(2)} €`);
 }
